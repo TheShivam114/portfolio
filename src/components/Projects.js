807b2bcf6976e575e4418198a6e14b1a110b3d24
TheShivam114 <shivam.ml114@gmail.com>
2025-09-12 17:16:32 +0530

    change in the projects file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,28 +4,27 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects = [
     {
       title: "Invoice Management System (React)",
       description:
-        "This system allows users to create, view, edit, and delete invoices, track payments, and generate PDF exports for easy sharing",
+        "This system allows users to create, view, edit, and delete invoices, track payments, and generate PDF exports for easy sharing .",
       imgUrl: projImg1,
     },
     {
       title: "Weather API App (Next.js)",
       description:
-        "A modern web application built with Next.js that fetches and displays real-time weather data from a public API",
+        "A modern web application built with Next.js that fetches and displays real-time weather data from a public API .",
       imgUrl: projImg2,
     },
     {
       title: "Portfolio React ",
       description:
-        "This is my personal portfolio built entirely with React.js, showcasing my skills and projects as a fullstack web developer",
+        "This is my personal portfolio built entirely with React.js, showcasing my skills and projects as a fullstack web developer .",
       imgUrl: projImg3,
     },
     // {
@@ -51,51 +50,64 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>A feature-rich web application developed using React, Next.js, Tailwind CSS, Bootstrap, HTML, and CSS, offering efficient invoice management, intuitive user interactions, and real-time weather updates, showcasing modern frontend development and API integration skills</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>Coming soon</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Coming soon</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    A feature-rich web application developed using React,
+                    Next.js, Tailwind CSS, Bootstrap, HTML, and CSS, offering
+                    efficient invoice management, intuitive user interactions,
+                    and real-time weather updates, showcasing modern frontend
+                    development and API integration skills
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>Coming soon</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>Coming soon</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
